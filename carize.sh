341d8cf8c73885cb6dd810646c91a390895c533f
nyakiomaina <carolinenyakio21@gmail.com>
2024-10-22 14:04:40 +0300

    dag stat json output

--- carize.sh
+++ carize.sh
@@ -1,6 +1,20 @@
 #!/bin/sh
+
 ipfs init
 ipfs add --cid-version=1 --raw-leaves=false -r -Q /data > /output/output.cid
+
+if [ ! -s /output/output.cid ]; then
+    echo "Error: Failed to retrieve CID."
+    exit 1
+fi
+
 CID=$(cat /output/output.cid)
-ipfs dag export `cat /output/output.cid` > /output/output.car
-#ipfs dag stat $CID --output=json > /output/output.car.json
+
+ipfs dag export "$CID" > /output/output.car
+
+if ipfs dag stat "$CID" --output=json > /output/output.car.json; then
+    echo "CAR and JSON files successfully created!"
+else
+    echo "Error: Failed to generate JSON file."
+    exit 1
+fi
