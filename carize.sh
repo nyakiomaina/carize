b4d3de594e39640bc1d7fe6f6a9e92c2ab70f330
nyakiomaina <carolinenyakio21@gmail.com>
2024-10-24 10:49:05 +0300

    fixes

--- carize.sh
+++ carize.sh
@@ -12,15 +12,15 @@ CID=$(cat /output/output.cid)
 
 ipfs dag export "$CID" > /output/output.car
 
-if ipfs --enc=json dag stat "$CID" > /output/output.car.json; then
+if ipfs --enc=json files stat "/ipfs/$CID" > /output/output.car.json; then
     echo "CAR and JSON files successfully created!"
 else
     echo "Error: Failed to generate JSON file."
     exit 1
 fi
 
-if jq -r '.TotalSize' /output/output.car.json > /output/output.dagsize; then
-    echo "TotalSize extracted to output.dagsize successfully!"
+if jq -r '.TotalSize' /output/output.car.json > /output/output.size; then
+    echo "TotalSize extracted to output.size successfully!"
 else
     echo "Error: Failed to extract TotalSize."
     exit 1
